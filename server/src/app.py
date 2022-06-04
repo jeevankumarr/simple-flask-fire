200053652a9b1a1af7f1e13c50dba11585ae98ee
Jeevan Ramakrishna <jeevan.kr@gmail.com>
2022-06-03 23:21:11 -0700

    first commit

---
+++ server/src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, make_response
+
+import os
+import time
+
+app = Flask(__name__)
+
+def get_time():
+    server_time = time.localtime()
+    return time.strftime('%I:%M:%S %p', server_time)
+
+@app.route('/')
+def index():
+    context = {'server_time': get_time()}
+    return render_template('index.html', context=context)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
